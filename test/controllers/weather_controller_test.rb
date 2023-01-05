daa9b50c4e8aaa71ac4a371a28f4bad872b126bc
Mayank Jethva <m.jethva01@gmail.com>
2023-01-05 14:10:42 -0600

    update title and add tests

--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class WeatherControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index return a successful response" do
+    get "/"
+    assert_response :success
+  end
+  test "search should return successful response" do
+    get "/search?address=1125+Goldfinch+Lane+Antioch+IL+60002&num_days=7&commit=Get+Weather"
+    assert_response :success
+  end
 end
