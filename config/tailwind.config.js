6767a6e4f5bd038f327ec13e1b42bdda0faef564
Mayank Jethva <m.jethva01@gmail.com>
2023-01-05 14:12:35 -0600

    fix tailwind warning

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,20 +1,18 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: {
-    safelist: [
-      'bg-sky-100',
-      'bg-sky-300',
-      'bg-amber-200',
-      'bg-orange-400',
-      'bg-red-500'
-    ],
-  },
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  safelist: [
+    'bg-sky-100',
+    'bg-sky-300',
+    'bg-amber-200',
+    'bg-orange-400',
+    'bg-red-500'
   ],
   theme: {
     extend: {
